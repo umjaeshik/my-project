5f9aa3f29e1d4815aa383f8f43836c50a99bf7b2
Um js <cute_js@daum.net>
2024-03-15 10:41:33 +0900

    add printf2

--- hello.c
+++ hello.c
@@ -2,9 +2,11 @@
 
 int main()
 {
-    printf('hello world');
+    printf('hello world0');
 
-    printf('hello world');
+    printf('hello world1');
+
+    printf('hello world2');
     return 0;
 
 }
